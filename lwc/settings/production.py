a4384a8a0949fa71595bacb03bfc6cef88c455c8
nasa <nasa.freaks@gmail.com>
2015-10-14 06:57:08 +0700

    change static dirs

--- lwc/settings/production.py
+++ lwc/settings/production.py
@@ -20,7 +20,7 @@ ALLOWED_HOSTS = ['*']
 # Static asset configuration
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = 'static_dirs'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
