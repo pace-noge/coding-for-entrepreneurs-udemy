5ecac2f88df714289a58e34a96d6e90a81304558
nasa <nasa.freaks@gmail.com>
2015-10-14 07:00:38 +0700

    change static files dirs on productin

--- lwc/settings/production.py
+++ lwc/settings/production.py
@@ -20,9 +20,11 @@ ALLOWED_HOSTS = ['*']
 # Static asset configuration
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-STATIC_ROOT = 'static_dirs'
+
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static", "static_root")
+
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, "static", "static_dirs"),
 )
